aff6c1daffa46b1f31c577663466f4f1df3e6f07
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-17 21:41:12 -0500

    21.12-dev-73

--- dist/js/script.js
+++ dist/js/script.js
@@ -59,63 +59,56 @@ API.Plugins.notes = {
 			}, 1000);
 		},
 	},
-	extend:{
-		clients:{
-			init:function(){
-				var url = new URL(window.location.href);
-				if((typeof url.searchParams.get("v") !== "undefined")&&(url.searchParams.get("v") == 'details')){
-					var checkExist = setInterval(function(){
-						if(($('[data-plugin="clients"][data-key="id"]').html() != '')&&($('#clientsTabs').find('.tab-content').length > 0)){
-							clearInterval(checkExist);
-							var id = $('[data-plugin="clients"][data-key="id"]').html();
-							API.Plugins.clients.Tabs.add('notes', function(tab){
-								API.request('notes','read',{
-									data:{filters:[
-										{ relationship:'equal', name:'relationship', value:'clients'},
-										{ relationship:'equal', name:'link_to', value:id},
-									]},
-									toast:false,
-									pace:false,
-								},function(result){
-									var dataset = JSON.parse(result), html = '', etoast = false;
-									if(typeof dataset.success !== 'undefined'){
-										for(const [key, value] of Object.entries(dataset.output.dom)){ API.Helper.set(API.Contents,['data','dom','notes',value.id],value); }
-										for(const [key, value] of Object.entries(dataset.output.raw)){ API.Helper.set(API.Contents,['data','raw','notes',value.id],value); }
-										API.Builder.table(tab, dataset.output.dom, {
-											headers:dataset.output.headers,
-											id:'ClientsNotes',
-											modal:true,
-											key:'id',
-											set:{
-												relationship:'clients',
-												link_to:id,
-												by:API.Contents.Auth.User.id,
-											},
-											plugin:'notes',
-											clickable:{
-												enable:true,
-												plugin:'notes',
-												view:'details',
-											},
-											predifined:{
-												relationship:'%plugin%',
-												link_to:'%id%',
-											},
-											import:{ key:'id', },
-											controls:{
-												toolbar:true,
-											},
-											modalWidth:'modal-lg',
-										}, function(table){
-											API.Plugins.notes.element.table.clients = table.table;
-										});
-									}
+	Timeline:{
+		icon:"history",
+		object:function(dataset,layout,options = {},callback = null){
+			if(options instanceof Function){ callback = options; options = {}; }
+			var defaults = {icon: API.Plugins.notes.Timeline.icon,color: "primary"};
+			if(API.Helper.isSet(options,['icon'])){ defaults.icon = options.icon; }
+			if(API.Helper.isSet(options,['color'])){ defaults.color = options.color; }
+			if(typeof dataset.id !== 'undefined'){
+				var dateItem = new Date(dataset.created);
+				var dateUS = dateItem.toLocaleDateString('en-US', {day: 'numeric', month: 'short', year: 'numeric'}).replace(/ /g, '-').replace(/,/g, '');
+				API.Builder.Timeline.add.date(layout.timeline,dataset.created);
+				var checkExist = setInterval(function() {
+					if(layout.timeline.find('div.time-label[data-dateus="'+dateUS+'"]').length > 0){
+						clearInterval(checkExist);
+						var html = '';
+						if(typeof dataset.from !== 'undefined') { var user = dataset.from; }
+						else if(typeof dataset.user !== 'undefined') { var user = dataset.user; }
+						else if(typeof dataset.owner !== 'undefined') { var user = dataset.owner; }
+						else { var user = ''; }
+						if((typeof dataset.subject !== 'undefined')&&(dataset.subject != null)&&(dataset.subject != "")) { var subject = dataset.subject; }
+						else if((typeof dataset.title !== 'undefined')&&(dataset.title != null)&&(dataset.title != "")) { var subject = dataset.title; }
+						else { var subject = ''; }
+						html += '<div data-type="'+icon+'" data-id="'+dataset.id+'" data-date="'+dateItem.getTime()+'">';
+							html += '<i class="fas fa-'+icon+' bg-'+color+'"></i>';
+							html += '<div class="timeline-item">';
+								html += '<span class="time bg-'+color+'"><i class="fas fa-clock mr-2"></i><time class="timeago" datetime="'+dataset.created.replace(/ /g, "T")+'">'+dataset.created+'</time></span>';
+								html += '<h3 class="timeline-header bg-'+color+'"><a class="mr-2">'+user+'</a>'+subject+'</h3>';
+								html += '<div class="timeline-body">'+dataset.content+'</div>';
+							html += '</div>';
+						html += '</div>';
+						layout.timeline.find('div.time-label[data-dateus="'+dateUS+'"]').after(html);
+						var element = layout.timeline.find('[data-type="'+icon+'"][data-id="'+dataset.id+'"]');
+						element.find('time').timeago();
+						var items = layout.timeline.children('div').detach().get();
+						items.sort(function(a, b){
+							return new Date($(b).data("date")) - new Date($(a).data("date"));
+						});
+						layout.timeline.append(items);
+						if(API.Auth.validate('plugin', 'notes', 4)){
+							$('<a class="time bg-warning pointer"><i class="fas fa-trash-alt"></i></a>').insertAfter(element.find('span.time.bg-warning'));
+							element.find('a.pointer').off().click(function(){
+								API.CRUD.delete.show({ keys:dataset,key:'id', modal:true, plugin:'notes' },function(note){
+									element.remove();
 								});
 							});
 						}
-					}, 100);
-				}
-			},
+						if(callback != null){ callback(element); }
+					}
+				}, 100);
+			}
 		},
 	},
 }
