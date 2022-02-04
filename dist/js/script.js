c2fc8ec33dfb6697c7432733847740b353ab073e
Louis Ouellet <louis@TheMacSwitch.local>
2022-02-04 16:21:39 -0500

    22.02-dev-93

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,4 @@
-API.Plugins.notes = {
+Engine.Plugins.notes = {
 	element:{
 		table:{
 			index:{},
@@ -14,19 +14,19 @@ API.Plugins.notes = {
 		},
 	},
 	init:function(){
-		API.GUI.Sidebar.Nav.add('Notes', 'development');
+		Engine.GUI.Sidebar.Nav.add('Notes', 'development');
 	},
 	load:{
 		index:function(){
-			API.Builder.card($('#pagecontent'),{ title: 'Notes', icon: 'notes'}, function(card){
-				API.request('notes','read',{
+			Engine.Builder.card($('#pagecontent'),{ title: 'Notes', icon: 'notes'}, function(card){
+				Engine.request('notes','read',{
 					data:{options:{ link_to:'NotesIndex',plugin:'notes',view:'index' }},
 				},function(result) {
 					var dataset = JSON.parse(result);
 					if(dataset.success != undefined){
-						for(const [key, value] of Object.entries(dataset.output.dom)){ API.Helper.set(API.Contents,['data','dom','notes',value.id],value); }
-						for(const [key, value] of Object.entries(dataset.output.raw)){ API.Helper.set(API.Contents,['data','raw','notes',value.id],value); }
-						API.Builder.table(card.children('.card-body'), dataset.output.dom, {
+						for(const [key, value] of Object.entries(dataset.output.dom)){ Engine.Helper.set(Engine.Contents,['data','dom','notes',value.id],value); }
+						for(const [key, value] of Object.entries(dataset.output.raw)){ Engine.Helper.set(Engine.Contents,['data','raw','notes',value.id],value); }
+						Engine.Builder.table(card.children('.card-body'), dataset.output.dom, {
 							headers:dataset.output.headers,
 							id:'NotesIndex',
 							modal:true,
@@ -35,7 +35,7 @@ API.Plugins.notes = {
 							controls:{ toolbar:true},
 							import:{ key:'id', },
 						},function(response){
-							API.Plugins.notes.element.table.index = response.table;
+							Engine.Plugins.notes.element.table.index = response.table;
 						});
 					}
 				});
@@ -49,10 +49,10 @@ API.Plugins.notes = {
 					values += $(this).html();
 				});
 				if(values == ''){
-					API.request('notes','read',{data:{id:id,key:'name'}},function(result){
+					Engine.request('notes','read',{data:{id:id,key:'name'}},function(result){
 						var dataset = JSON.parse(result);
 						if(dataset.success != undefined){
-							API.GUI.insert(dataset.output.dom);
+							Engine.GUI.insert(dataset.output.dom);
 						}
 					});
 				}
@@ -63,16 +63,16 @@ API.Plugins.notes = {
 		icon:"sticky-note",
 		object:function(dataset,layout,options = {},callback = null){
 			if(options instanceof Function){ callback = options; options = {}; }
-			var defaults = {icon: API.Plugins.notes.Timeline.icon,color: "warning"};
-			for(var [key, option] of Object.entries(options)){ if(API.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
+			var defaults = {icon: Engine.Plugins.notes.Timeline.icon,color: "warning"};
+			for(var [key, option] of Object.entries(options)){ if(Engine.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
 			if(typeof dataset.id !== 'undefined'){
 				var dateItem = new Date(dataset.created);
 				var dateUS = dateItem.toLocaleDateString('en-US', {day: 'numeric', month: 'short', year: 'numeric'}).replace(/ /g, '-').replace(/,/g, '');
-				API.Builder.Timeline.add.date(layout.timeline,dataset.created);
+				Engine.Builder.Timeline.add.date(layout.timeline,dataset.created);
 				var checkExist = setInterval(function() {
 					if(layout.timeline.find('div.time-label[data-dateus="'+dateUS+'"]').length > 0){
 						clearInterval(checkExist);
-						API.Builder.Timeline.add.fileter(layout,'notes','notes');
+						Engine.Builder.Timeline.add.fileter(layout,'notes','notes');
 						var html = '';
 						if(typeof dataset.from !== 'undefined') { var user = dataset.from; }
 						else if(typeof dataset.user !== 'undefined') { var user = dataset.user; }
@@ -97,10 +97,10 @@ API.Plugins.notes = {
 							return new Date($(b).data("date")) - new Date($(a).data("date"));
 						});
 						layout.timeline.append(items);
-						if(API.Auth.validate('plugin', 'notes', 4)){
+						if(Engine.Auth.validate('plugin', 'notes', 4)){
 							$('<a class="time text-dark pointer"><i class="fas fa-trash-alt"></i></a>').insertAfter(element.find('span.time'));
 							element.find('a.pointer').off().click(function(){
-								API.CRUD.delete.show({ keys:dataset,key:'id', modal:true, plugin:'notes' },function(note){
+								Engine.CRUD.delete.show({ keys:dataset,key:'id', modal:true, plugin:'notes' },function(note){
 									element.remove();
 								});
 							});
@@ -116,13 +116,13 @@ API.Plugins.notes = {
 			tab:function(data,layout,options = {},callback = null){
 				if(options instanceof Function){ callback = options; options = {}; }
 				var defaults = {field: "name"};
-				for(var [key, option] of Object.entries(options)){ if(API.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
-				if(!API.Helper.isSet(layout,['content','notes'])){
-					API.GUI.Layouts.details.tab(data,layout,{icon:"fas fa-sticky-note",text:API.Contents.Language["Notes"]},function(data,layout,tab,content){
-						API.Builder.Timeline.add.filter(layout,'notes','Notes');
+				for(var [key, option] of Object.entries(options)){ if(Engine.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
+				if(!Engine.Helper.isSet(layout,['content','notes'])){
+					Engine.GUI.Layouts.details.tab(data,layout,{icon:"fas fa-sticky-note",text:Engine.Contents.Language["Notes"]},function(data,layout,tab,content){
+						Engine.Builder.Timeline.add.filter(layout,'notes','Notes');
 						layout.content.notes = content;
 						layout.tabs.notes = tab;
-						if(API.Auth.validate('plugin', 'notes', 2)){
+						if(Engine.Auth.validate('plugin', 'notes', 2)){
 							layout.content.notes.append('<div><textarea title="Note" name="note" class="form-control"></textarea></div>');
 							layout.content.notes.find('textarea').summernote({
 								toolbar: [
@@ -136,14 +136,14 @@ API.Plugins.notes = {
 							var html = '';
 							html += '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">';
 								html += '<form class="form-inline my-2 my-lg-0 ml-auto">';
-									html += '<button class="btn btn-warning my-2 my-sm-0" type="button" data-action="reply"><i class="fas fa-sticky-note mr-1"></i>'+API.Contents.Language['Add Note']+'</button>';
+									html += '<button class="btn btn-warning my-2 my-sm-0" type="button" data-action="reply"><i class="fas fa-sticky-note mr-1"></i>'+Engine.Contents.Language['Add Note']+'</button>';
 								html += '</form>';
 							html += '</nav>';
 							layout.content.notes.append(html);
 						}
 					});
 				}
-				API.Plugins.notes.Layouts.details.Events(data,layout);
+				Engine.Plugins.notes.Layouts.details.Events(data,layout);
 				if(callback != null){ callback(dataset,layout); }
 			},
 			GUI:{},
@@ -151,12 +151,12 @@ API.Plugins.notes = {
 				var url = new URL(window.location.href);
 				if(options instanceof Function){ callback = options; options = {}; }
 				var defaults = {field: "name"};
-				for(var [key, option] of Object.entries(options)){ if(API.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
-				if(API.Auth.validate('plugin', 'notes', 2)){
+				for(var [key, option] of Object.entries(options)){ if(Engine.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
+				if(Engine.Auth.validate('plugin', 'notes', 2)){
 					layout.content.notes.find('button').off().click(function(){
 					  if(!layout.content.notes.find('textarea').summernote('isEmpty')){
 					    var note = {
-					      by:API.Contents.Auth.User.id,
+					      by:Engine.Contents.Auth.User.id,
 					      content:layout.content.notes.find('textarea').summernote('code'),
 					      relationship:url.searchParams.get("p"),
 					      link_to:dataset.this.dom.id,
@@ -174,10 +174,10 @@ API.Plugins.notes = {
 					      ],
 					      height: 250,
 					    });
-					    API.request(url.searchParams.get("p"),'note',{data:note},function(result){
+					    Engine.request(url.searchParams.get("p"),'note',{data:note},function(result){
 					      var data = JSON.parse(result);
 					      if(data.success != undefined){
-									API.Plugins.notes.Timeline.object(data.output.note.dom,layout);
+									Engine.Plugins.notes.Timeline.object(data.output.note.dom,layout);
 					      }
 					    });
 					    layout.tabs.find('a').first().tab('show');
@@ -192,7 +192,7 @@ API.Plugins.notes = {
 					      ],
 					      height: 250,
 					    });
-					    alert(API.Contents.Language['Note is empty']);
+					    alert(Engine.Contents.Language['Note is empty']);
 					  }
 					});
 				}
@@ -201,4 +201,4 @@ API.Plugins.notes = {
 	},
 }
 
-API.Plugins.notes.init();
+Engine.Plugins.notes.init();
